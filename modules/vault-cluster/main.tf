89a91fcb530b665824e6bce4860d60e3dc730824
mekhatib <melkhatib@ziggo.nl>
2021-04-07 11:57:33 +0200

    Update main.tf

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -32,7 +32,7 @@ resource "azurerm_lb" "vault_access" {
 
   frontend_ip_configuration {
     name = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.vault_access[count.index]
+    public_ip_address_id = azurerm_public_ip.vault_access.id
   }
 }
 
@@ -50,7 +50,7 @@ resource "azurerm_lb_nat_pool" "vault_lbnatpool" {
 
 resource "azurerm_lb_probe" "vault_probe" {
   resource_group_name = "${var.resource_group_name}"
-  loadbalancer_id = azurerm_lb.vault_access[count.index]
+  loadbalancer_id = azurerm_lb.vault_access.id
   name                = "vault-running-probe"
   port                = "${var.api_port}"
 }
