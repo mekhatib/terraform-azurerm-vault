e3251324f08cc9e1872de028b312b142733c5f92
mekhatib <melkhatib@ziggo.nl>
2021-04-07 10:16:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,10 +7,10 @@
 # ---------------------------------------------------------------------------------------------------------------------
 
 provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id = "${var.client_id}"
-  client_secret = "${var.secret_access_key}"
-  tenant_id = "${var.tenant_id}"
+  #subscription_id = "${var.subscription_id}"
+  #client_id = "${var.client_id}"
+  #client_secret = "${var.secret_access_key}"
+  #tenant_id = "${var.tenant_id}"
 }
 
 terraform {
