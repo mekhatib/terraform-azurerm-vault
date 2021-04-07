9b7948e7712b4855fb8b706db7363e6c9352d5de
mekhatib <melkhatib@ziggo.nl>
2021-04-07 12:52:10 +0200

    Update main.tf

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -67,7 +67,7 @@ resource "azurerm_lb_rule" "vault_api_port" {
   count = "${var.associate_public_ip_address_load_balancer ? 1 : 0}"
   resource_group_name = "${var.resource_group_name}"
   name = "vault-api"
-  loadbalancer_id = "${azurerm_lb.vault_access.id}"
+  loadbalancer_id = azurerm_lb.vault_access[count.index].id
   protocol = "Tcp"
   frontend_port = "${var.api_port}"
   backend_port = "${var.api_port}"
