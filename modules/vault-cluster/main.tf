49f17a9e004a20f1735ec5f1517adbafb4e58da4
mekhatib <melkhatib@ziggo.nl>
2021-04-07 11:45:38 +0200

    Update main.tf

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -7,7 +7,7 @@ terraform {
 # ---------------------------------------------------------------------------------------------------------------------
 resource "azurerm_storage_container" "vault" {
   name                  = "${var.storage_container_name}"
-  resource_group_name   = "${var.resource_group_name}"
+  #resource_group_name   = "${var.resource_group_name}"
   storage_account_name  = "${var.storage_account_name}"
   container_access_type = "private"
 }
