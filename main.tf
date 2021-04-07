6bae3401b297178da755f9cb5b77c360559226ea
mekhatib <melkhatib@ziggo.nl>
2021-04-07 11:46:40 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ data "template_file" "custom_data_consul" {
 # ---------------------------------------------------------------------------------------------------------------------
 resource "azurerm_storage_container" "vault_storage" {
   name                  = "vault"
-  resource_group_name   = "${var.resource_group_name}"
+  #resource_group_name   = "${var.resource_group_name}"
   storage_account_name  = "${var.storage_account_name}"
   container_access_type = "private"
 }
