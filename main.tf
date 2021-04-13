4a379b1977e0fac4b98361d6b2514b52e64e3df5
mekhatib <melkhatib@ziggo.nl>
2021-04-13 14:12:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ terraform {
 #  resource_group_name = "${var.resource_group_name}"
 #  virtual_network_name = "${azurerm_virtual_network.consul.name}"
 #  address_prefix = "${var.subnet_address}"
-}
+#}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # DEPLOY THE CONSUL SERVER NODES
